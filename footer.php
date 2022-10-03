925a733b5873b42d2946e4cc827c459775777259
giangcute <giagthuy34@gmail.com>
2022-10-03 09:59:35 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div class="jumbotron">
+     
+<div class="appeal">            
+        </div>
+ 
+<h2>Copyright @ 2020 Design Giang</h2> 
+  
+</div>
+ 
+</div>
+ 
+<!--/. Container-->
+</div>
+ 
+</div>
+ 
+</body>
+</html>
